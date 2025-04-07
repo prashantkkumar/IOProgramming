48509cb0ab126837484da5b00fadee24cb817b08
Prashant <prashantoff61@gmail.com>
2025-04-07 13:26:17 +0530

    [Prashant] add.Filter JSON data: Print only users older than 25 years

---
+++ src/JSON_OBJECT/src/main/java/PracticeHandOnProblem/FilterJSON/filterjson.java
@@ -0,0 +1,39 @@
+package PracticeHandOnProblem.FilterJSON;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class filterjson {
+    public static void main(String[] args)throws Exception {
+        String path = new String(
+                Files.readAllBytes(Paths.get("C:\\Users\\Prashant Kumar\\OneDrive\\Desktop\\JAVA\\week5\\src\\JSON_OBJECT\\src\\main\\java\\Practice_Problems\\FilterJSON\\Data.json"))
+        );
+
+
+        JSONObject obj = new JSONObject(path);
+
+
+        JSONArray users = obj.getJSONArray("users");
+
+
+        JSONArray filtered = new JSONArray();
+
+        for (int i = 0; i < users.length(); i++) {
+            JSONObject user = users.getJSONObject(i);
+            int age = user.getInt("age");
+
+            if (age > 25) {
+                filtered.put(user);
+            }
+        }
+
+
+        JSONObject result = new JSONObject();
+        result.put("users_under_25", filtered);
+
+        System.out.println(result.toString(2));
+    }
+}
